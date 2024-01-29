2f684f91b66c8869dce5c0789ec724de13709ebd
slava <slavavisuals@gmail.com>
2024-01-29 16:21:07 -0700

    added changes in config

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,9 +2,6 @@ import MemberProfile from './MemberProfile';
 import NavLinks from './NavLinks';
 import SidebarHeader from './SidebarHeader';
 
-// const dynamic = 'force-dynamic';
-// const revalidate = 0;
-
 const Sidebar = () => {
   return (
     <div className='grid grid-rows-[auto,1fr,auto] min-h-full px-4 py-12 w-80 bg-base-300'>
